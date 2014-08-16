9df33c1bb76248c0cc04e5aee6fddfb52aa6c3f1
Yasyf Mohamedali <yasyfm@gmail.com>
2014-08-15 23:02:35 -0400

    better init script and update 1 & 2

--- 1/1.rb
+++ 1/1.rb
@@ -1,5 +1,14 @@
 #!/usr/bin/env ruby
 
+"""
+Problem 1
+Multiples of 3 and 5
+Published on 05 October 2001 at 06:00 pm [Server Time]
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
 def main
   (0..999).reduce do |total, n|
     [n%3, n%5].include?(0) ? total + n : total
@@ -7,4 +16,3 @@ def main
 end
 
 puts main()
-
--- create.rb
+++ create.rb
@@ -1,24 +1,37 @@
 #!/usr/bin/env ruby
-
+require 'rest_client'
 require 'fileutils'
+require 'ostruct'
+require 'erb'
+require 'json'
 
-TEMPLATE ="""#!/usr/bin/env ruby
-
-def main
-
+class ProjectEulerQuestion < OpenStruct
+  def initialize(n)
+    response = RestClient.get "https://www.kimonolabs.com/api/735b3r5e?apikey=c70ab7eb0f58c87669ff7bf74b2dc551&kimpath1=problem%3D#{n}"
+    data = JSON.parse(response)['results']['question']
+    h = data.first
+    @problem = h['problem']
+    @title = h['title']
+    @date = h['date']
+    @question = data.map { |r| r['question']  }.join("\n")
+  end
+  def render
+    ERB.new(template).result(binding)
+  end
+  def template
+    File.open('template.rb.erb', 'r').read
+  end
 end
 
-puts main()
-
-"""
 
 def main
   n = ARGV.last.to_s
-  FileUtils.mkdir n
+  FileUtils.mkdir_p n
   File.open("#{n}/#{n}.rb", 'w') do |f|
-    f.write TEMPLATE
+    f.write ProjectEulerQuestion.new(n).render
   end
   `subl #{n}/#{n}.rb`
+  `open https://projecteuler.net/problem=#{n}`
 end
 
 main()
